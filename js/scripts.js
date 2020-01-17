84bcabbb5b72a2558951aca69860eae9777fa920
Adela <adela.yohana@gmail.com>
2020-01-17 11:31:09 -0800

    Add initial branching logic for scoring

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,18 @@ $(document).ready(function() {
     var answer3 = parseInt($("input:radio[name=question3radios]:checked").val());
     var answer4 = parseInt($("input:radio[name=question4radios]:checked").val());
     var answer5 = parseInt($("input:radio[name=question5radios]:checked").val());
-    alert(answer1+answer2+answer3+answer4+answer5);
+    var score = answer1 + answer2 + answer3 + answer4 + answer5;
+    alert(score);
+
+    if (score >= 40) {
+      alert("JS");
+    } else if (score >= 25 && score < 40) {
+      alert("Ruby");
+    } else if (score <= 15) {
+      alert("C#");
+    } else {
+      alert("please retry");
+    }
 
 
   });
