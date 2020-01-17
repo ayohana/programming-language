5a21a1f15e02fd56245dfb899507002ca0a8b298
Adela <adela.yohana@gmail.com>
2020-01-17 11:26:26 -0800

    Confirm reading of all form inputs as integers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $("form#questionsList").submit(function(event) {
     event.preventDefault();
-    var answer1 = $("input:radio[name=question1radios]:checked").val();
-    var answer2 = $("input:radio[name=question2radios]:checked").val();
-    var answer3 = $("input:radio[name=question3radios]:checked").val();
-    alert(answer1 + answer2 + answer3);
+    var answer1 = parseInt($("input:radio[name=question1radios]:checked").val());
+    var answer2 = parseInt($("input:radio[name=question2radios]:checked").val());
+    var answer3 = parseInt($("input:radio[name=question3radios]:checked").val());
+    var answer4 = parseInt($("input:radio[name=question4radios]:checked").val());
+    var answer5 = parseInt($("input:radio[name=question5radios]:checked").val());
+    alert(answer1+answer2+answer3+answer4+answer5);
 
-    
+
   });
 });
